532a36a72faaeec06b977a8b88f1908dec166eee
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2023-08-26 11:16:54 -0500

    merge environment variable

--- users/src/main.py
+++ users/src/main.py
@@ -4,16 +4,16 @@ from .blueprints.user import users_blueprint
 from dotenv import load_dotenv
 from flask import Flask, jsonify
 from .errors.errors import ApiError
-
+os.getenv('ENV')
 app = Flask(__name__)
 app.register_blueprint(users_blueprint)
 loaded = load_dotenv('.env.development')
 
-
+print(os.environ["VERSION"])
 @app.errorhandler(ApiError)
 def handle_exception(err):
     response = {
         "mssg": err.description,
-        #"version": os.environ["VERSION"]
+        "version": os.environ["VERSION"]
     }
     return jsonify(response), err.code
