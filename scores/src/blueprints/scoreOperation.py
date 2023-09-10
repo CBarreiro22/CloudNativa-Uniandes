be9c2254b65cab71427a4479995bd68547b63b64
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2023-09-09 21:35:07 -0500

    Update score

--- scores/src/blueprints/scoreOperation.py
+++ scores/src/blueprints/scoreOperation.py
@@ -1,4 +1,5 @@
 
+import os
 import requests
 from flask import Blueprint, jsonify, request
 from src.models.model import init_db
@@ -6,6 +7,8 @@ from src.models.model import init_db
 
 # Crear el Blueprint para el calculo del score
 scores_blueprint = Blueprint('scores', __name__)
+OFFER_PATH = os.environ["OFFERS_PATH"]
+ROUTE_PATH = os.environ["ROUTE_PATH"]
 
 init_db()
 
@@ -32,44 +35,40 @@ def score_operation():
         "Authorization": token
     }
 
-    # Realizar una solicitud GET a la ruta /offers/id_offer
-    offer_response = requests.get(f"http://192.168.1.58:3003/offers/{id_offer}", headers=headers)
-    print(offer_response)
+    try:
 
-    if offer_response.status_code != 200:
-        return jsonify({"error": "Error al obtener la oferta"}), 500
+        # Realizar una solicitud GET a la ruta /offers/id_offer
+        offer_response = requests.get(f"{OFFER_PATH}/offers/{id_offer}", headers=headers)
+        print(offer_response.status_code)
+        if offer_response.status_code != 200:
+            if offer_response.status_code == 401:
+                return jsonify({"error": "Token invalido o esta vencido"}), 401
+            return jsonify({"error": "Error al obtener la oferta"}), 500
 
-    offer_data = offer_response.json()
-    print(offer_data)
-    offer_value = offer_data.get("offer")
-    print(offer_value)
-    offer_size = offer_data.get("size")
-    print(offer_size)
+        offer_data = offer_response.json()
+        offer_value = offer_data.get("offer")
+        offer_size = offer_data.get("size")
 
-    # Realizar una solicitud GET a la ruta /routes/id_route
-    route_response = requests.get(f"http://192.168.1.58:3002/routes/{id_route}", headers=headers)
+        # Realizar una solicitud GET a la ruta /routes/id_route
+        route_response = requests.get(f"{ROUTE_PATH}/routes/{id_route}", headers=headers)
 
-    if route_response.status_code != 200:
-        return jsonify({"error": "Error al obtener la ruta"}), 500
+        if route_response.status_code != 200:
+            return jsonify({"error": "Error al obtener la ruta"}), 500
 
-    route_data = route_response.json()
-    bag_cost = route_data.get("bagCost")
+        route_data = route_response.json()
+        bag_cost = route_data.get("bagCost")
 
-    # Realizar el cálculo del score utilizando los valores obtenidos
-    score = calcular_score(offer_value, offer_size, bag_cost)
-    print(score)
-    # Devolver el score calculado como respuesta
-    return jsonify({"score": score}), 200
-   # try:
+        # Realizar el cálculo del score utilizando los valores obtenidos
+        score = calcular_score(offer_value, offer_size, bag_cost)
 
-    #except Exception as e:
-    #    return jsonify({"error": "Error interno del servidor"}), 500
+        # Devolver el score calculado como respuesta
+        return jsonify({"score": score}), 200
+   
+
+    except Exception as e:
+        return jsonify({"error": "Error interno del servidor"}), 500
 
 def calcular_score(offer_value, offer_size, bag_cost):
-    print("ingreso a calcular score")
-    print(offer_value,"offer value")
-    print(offer_size,"offer size")
-    print(bag_cost,"bag cost")
     # monto oferta - (porcentaje de ocupación de una maleta * valor de la maleta en el trayecto)
     if offer_size == 'SMALL':
         procentagesBag =1
