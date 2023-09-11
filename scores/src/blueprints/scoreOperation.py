b1b374372b8b1b4cbc085e09dacdaa43592de91e
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2023-09-11 12:30:33 -0500

    Score get

--- scores/src/blueprints/scoreOperation.py
+++ scores/src/blueprints/scoreOperation.py
@@ -2,7 +2,10 @@
 import os
 import requests
 from flask import Blueprint, jsonify, request
-from src.models.model import init_db
+
+from ..models.score import Scores
+from ..errors.errors import TokenNotHeaderError
+from src.models.model import init_db,db_session
 
 
 # Crear el Blueprint para el calculo del score
@@ -35,38 +38,50 @@ def score_operation():
         "Authorization": token
     }
 
-    try:
 
-        # Realizar una solicitud GET a la ruta /offers/id_offer
-        offer_response = requests.get(f"{OFFER_PATH}/offers/{id_offer}", headers=headers)
-        print(offer_response.status_code)
-        if offer_response.status_code != 200:
-            if offer_response.status_code == 401:
-                return jsonify({"error": "Token invalido o esta vencido"}), 401
-            return jsonify({"error": "Error al obtener la oferta"}), 500
+    # Realizar una solicitud GET a la ruta /offers/id_offer
+    offer_response = requests.get(f"{OFFER_PATH}/offers/{id_offer}", headers=headers)
+    print(offer_response.status_code)
+    if offer_response.status_code != 200:
+        if offer_response.status_code == 401:
+            return jsonify({"error": "Token invalido o esta vencido"}), 401
+        return jsonify({"error": "Error al obtener la oferta"}), 500
 
-        offer_data = offer_response.json()
-        offer_value = offer_data.get("offer")
-        offer_size = offer_data.get("size")
+    offer_data = offer_response.json()
+    offer_value = offer_data.get("offer")
+    offer_size = offer_data.get("size")
 
-        # Realizar una solicitud GET a la ruta /routes/id_route
-        route_response = requests.get(f"{ROUTE_PATH}/routes/{id_route}", headers=headers)
+    # Realizar una solicitud GET a la ruta /routes/id_route
+    route_response = requests.get(f"{ROUTE_PATH}/routes/{id_route}", headers=headers)
 
-        if route_response.status_code != 200:
-            return jsonify({"error": "Error al obtener la ruta"}), 500
+    if route_response.status_code != 200:
+        return jsonify({"error": "Error al obtener la ruta"}), 500
 
-        route_data = route_response.json()
-        bag_cost = route_data.get("bagCost")
+    route_data = route_response.json()
+    bag_cost = route_data.get("bagCost")
 
-        # Realizar el cálculo del score utilizando los valores obtenidos
-        score = calcular_score(offer_value, offer_size, bag_cost)
+    # Realizar el cálculo del score utilizando los valores obtenidos
+    score = calcular_score(offer_value, offer_size, bag_cost)
+    nuevo_score = Scores(
+        id_offer=id_offer,
+        id_route=id_route,
+        offer=offer_value,
+        size=offer_size,
+        bagcost=bag_cost,
+        score=score
+    )
+    print(nuevo_score)
+    db_session.add(nuevo_score)
 
-        # Devolver el score calculado como respuesta
-        return jsonify({"score": score}), 200
+    db_session.commit()
+
+    # Devolver el score calculado como respuesta
+    return jsonify({"score": score}), 200
    
 
-    except Exception as e:
-        return jsonify({"error": "Error interno del servidor"}), 500
+    #try:
+    #except Exception as e:
+     #   return jsonify({"error": "Error interno del servidor"}), 500
 
 def calcular_score(offer_value, offer_size, bag_cost):
     # monto oferta - (porcentaje de ocupación de una maleta * valor de la maleta en el trayecto)
@@ -82,3 +97,33 @@ def calcular_score(offer_value, offer_size, bag_cost):
 
     return utility
 
+@scores_blueprint.route('/score/<string:id_offer>', methods=['GET'])
+def get_score_info(id_offer):
+    # Obtener el token del encabezado Authorization
+    token = request.headers.get('Authorization')
+    print("INgreso al metodo")
+    if not token or not token.startswith('Bearer '):
+        raise TokenNotHeaderError("El token no está en el encabezado de la solicitud")
+
+    print(id_offer,"id_offer")
+    token = token.split(' ')[1]
+    print(token,"token")
+    score = db_session.query(Scores).filter_by(id_offer=id_offer).first()
+    print(score,"score")
+    if score is None:
+        return jsonify({"error": "Puntaje no encontrado"}), 404  # Devuelve un error 404 si no se encuentra el puntaje
+    response = {
+        "id": str(score.id),
+        "Score": score.score
+    }
+    return jsonify(response), 200
+
+@scores_blueprint.route('/score/reset', methods=['POST'])
+def reset_database():
+    # Eliminar todos los registros de la tabla Users
+    db_session.query(Scores).delete()
+
+    # Realizar commit y cerrar la sesión
+    db_session.commit()
+
+    return jsonify({"msg": "Todos los datos fueron eliminados"}), 200
--- scores/src/models/score.py
+++ scores/src/models/score.py
@@ -1,23 +1,26 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Double, Integer, String
 from src.models.model import Model
 
 
 class Scores(Model):
     __tablename__ = 'scores'
 
-
+    id_offer = Column(String, name='id_offer', doc="id de la oferta")
+    id_route = Column(String, name='id_route', doc="id del trayecto")
     offer = Column(Integer, doc="valor en dólares de la oferta por llevar el paquete")
     size = Column(String, name='size',
                   doc="un valor que describe subjetivamente del tamaño del paquete, puede ser LARGE,MEDIUM,SMALL")
     bagCost = Column(Integer, doc="costo del envío de una maleta en el trayecto")
-    scores = Column(Integer,
+    score = Column(Double, name='score',
                     doc="ganancia aproximada sobre una oferta realizada.(utilidad = monto oferta - (porcentaje de ocupación de una maleta * valor de la maleta en el trayecto))")
 
-    def __init__(self, offer, size, bagcost, scores):
+    def __init__(self, id_offer,id_route,offer, size, bagcost, score):
+        self.id_offer = id_offer
+        self.id_route = id_route
         self.offer = offer
         self.size = size
         self.bagCost = bagcost
-        self.scores = scores
+        self.score = score
 
         super().__init__()
 
